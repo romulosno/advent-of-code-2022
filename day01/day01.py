1394cffa8b5bfda3a30e6aae719a661ac4321326
Romulo Souza <romulosnoliveira@gmail.com>
2022-12-01 21:41:31 -0300

    Dia 1

---
+++ day01/day01.py
@@ -0,0 +1,17 @@
+# Using readlines()
+file1 = open('input01', 'r')
+Lines = file1.readlines()
+totais = []
+
+count = 0
+for line in Lines:
+    if line.strip() == "":
+        totais.insert(count, 0)
+        count = count + 1
+    else:
+        totalAtual = totais[count-1]
+        totais.insert(count-1, totalAtual+int(line.strip()))
+
+totais.sort(reverse=True)
+print(totais[0])
+print(totais[0]+totais[1]+totais[2])
